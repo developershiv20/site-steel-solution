057bc4f915f782eb96e682cd93db20ed009bfbf0
developershiv20 <developer.shiv2020@gmail.com>
2020-08-04 02:00:50 +0530

    code uploaded

---
+++ dist/precache-manifest.cdb6d6dacd41f94ef90a32d9741727b0.js
@@ -0,0 +1,282 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f133cc58e59546bc3825d620a79c6447",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c64267ccbe5cf2b9f313",
+    "url": "/static/css/2.af723d61.chunk.css"
+  },
+  {
+    "revision": "10093f1f5cfe4fd14545",
+    "url": "/static/css/main.be8c85fa.chunk.css"
+  },
+  {
+    "revision": "c64267ccbe5cf2b9f313",
+    "url": "/static/js/2.fbb7fe4d.chunk.js"
+  },
+  {
+    "revision": "262d580f1fa293bd65b1d554bcd9b715",
+    "url": "/static/js/2.fbb7fe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10093f1f5cfe4fd14545",
+    "url": "/static/js/main.4b22c45a.chunk.js"
+  },
+  {
+    "revision": "967221aa8b762f02730a",
+    "url": "/static/js/runtime-main.130dca70.js"
+  },
+  {
+    "revision": "6e73f917738a1a8e357def461be690cb",
+    "url": "/static/media/10.6e73f917.jpg"
+  },
+  {
+    "revision": "1d7023e18d6ad65a9741f16535d1d960",
+    "url": "/static/media/11.1d7023e1.jpg"
+  },
+  {
+    "revision": "c788a843013e40d0a3fdcb515fc32811",
+    "url": "/static/media/113.c788a843.jpeg"
+  },
+  {
+    "revision": "673fbefcb538857d37e64584c1b9a3dd",
+    "url": "/static/media/12.673fbefc.jpg"
+  },
+  {
+    "revision": "72ab91eb976753b2c2739929368a0df6",
+    "url": "/static/media/13.72ab91eb.jpg"
+  },
+  {
+    "revision": "eda6e217750daba06870e0c3b5a8f69e",
+    "url": "/static/media/15.eda6e217.jpg"
+  },
+  {
+    "revision": "ce2dd01c721b30a49c11606f93666811",
+    "url": "/static/media/2.ce2dd01c.jpg"
+  },
+  {
+    "revision": "9822f419856bdbe7c9b20d7f4ef61c76",
+    "url": "/static/media/6.9822f419.jpg"
+  },
+  {
+    "revision": "3a03ad091afc812e04fb8bf9629e5f9d",
+    "url": "/static/media/7.3a03ad09.jpg"
+  },
+  {
+    "revision": "9c6892b0009ef0bc012bb36194dca457",
+    "url": "/static/media/8.9c6892b0.jpg"
+  },
+  {
+    "revision": "baf0ee85b1611d4b539b7e078dba64a6",
+    "url": "/static/media/9.baf0ee85.jpg"
+  },
+  {
+    "revision": "e9dd1c1e6a3d53b4269fb508f04444e5",
+    "url": "/static/media/Bhushan-Steel-Limited.e9dd1c1e.jpg"
+  },
+  {
+    "revision": "f35e96347106aeddfec478f278bdb76e",
+    "url": "/static/media/Company Profile.f35e9634.pdf"
+  },
+  {
+    "revision": "78e9a8ac4c0d0cb1e63a61476bb77f9e",
+    "url": "/static/media/GICO.78e9a8ac.png"
+  },
+  {
+    "revision": "e3b5febbf2033bb30b08831bbc6f06a6",
+    "url": "/static/media/IMG_0973-removebg-preview.e3b5febb.png"
+  },
+  {
+    "revision": "d99a8b2d01cfd34de27b8209e47e68d8",
+    "url": "/static/media/L&T.d99a8b2d.png"
+  },
+  {
+    "revision": "95b662de4abf0b03aff13ddd4db9097a",
+    "url": "/static/media/Monnet.95b662de.jpg"
+  },
+  {
+    "revision": "b7d1afd15f83db4efce27e2e622dbe2a",
+    "url": "/static/media/OCL.b7d1afd1.png"
+  },
+  {
+    "revision": "2371feea39931a4bec2c852d22a4b709",
+    "url": "/static/media/SBQ.2371feea.png"
+  },
+  {
+    "revision": "72b93ab7bcb22fdec73e57078d329c6a",
+    "url": "/static/media/Saurabh.72b93ab7.jpg"
+  },
+  {
+    "revision": "a5e740e7ef03274bb728403491eff612",
+    "url": "/static/media/Shubham.a5e740e7.jpg"
+  },
+  {
+    "revision": "6d49dc1aa6b22775b8dea0b9224170ad",
+    "url": "/static/media/VallabhSteel.6d49dc1a.png"
+  },
+  {
+    "revision": "b2240531e4d876da56c4b6cdcc8a2449",
+    "url": "/static/media/adhunik.b2240531.png"
+  },
+  {
+    "revision": "c7427c0f84917bdd45ebf23a2ba74343",
+    "url": "/static/media/aman.c7427c0f.jpeg"
+  },
+  {
+    "revision": "8a5597b96ae6fc5acd9417dd44e77dc6",
+    "url": "/static/media/arcelor .8a5597b9.png"
+  },
+  {
+    "revision": "a8b40c30f8ac8443a2f29248bda82531",
+    "url": "/static/media/aryan.a8b40c30.jpeg"
+  },
+  {
+    "revision": "4e4d2b3d3a1c24fb1fbebd6ebc1bcf31",
+    "url": "/static/media/banner1.4e4d2b3d.jpg"
+  },
+  {
+    "revision": "8bc69de5620288cc9d12593610fe7f87",
+    "url": "/static/media/civilworkbanner.8bc69de5.jpeg"
+  },
+  {
+    "revision": "9c3719a5c1e0d4c58bcee0e3d1012601",
+    "url": "/static/media/clientbanner.9c3719a5.jpeg"
+  },
+  {
+    "revision": "c453734bc4c2c4ae61d2c37529e005a3",
+    "url": "/static/media/contactbanner.c453734b.jpg"
+  },
+  {
+    "revision": "a83184ecbafbe8883e8804779ff8d93e",
+    "url": "/static/media/ed.a83184ec.jpeg"
+  },
+  {
+    "revision": "2fd6be5c60b563826917eb39ed97eda1",
+    "url": "/static/media/fb.2fd6be5c.jpeg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a3eb6b1feb423397449258f7a682cc0b",
+    "url": "/static/media/g1.a3eb6b1f.jpeg"
+  },
+  {
+    "revision": "c76540147603157b565cfa66b3447a49",
+    "url": "/static/media/g10.c7654014.jpeg"
+  },
+  {
+    "revision": "36bb76dbbb9655de2f6fb0845b187fc0",
+    "url": "/static/media/g11.36bb76db.jpeg"
+  },
+  {
+    "revision": "0e3b31e3a235ff5b7bb672b4653fb277",
+    "url": "/static/media/g12.0e3b31e3.jpeg"
+  },
+  {
+    "revision": "da0ca5392948b59800a5a1422214502c",
+    "url": "/static/media/g2.da0ca539.jpeg"
+  },
+  {
+    "revision": "29ddf5410a660379478296790dbda415",
+    "url": "/static/media/g3.29ddf541.jpeg"
+  },
+  {
+    "revision": "82fcf69b839319c158e428c788fbb3b9",
+    "url": "/static/media/g4.82fcf69b.JPG"
+  },
+  {
+    "revision": "928bf9dec89004e98c25a53af81c030f",
+    "url": "/static/media/g5.928bf9de.JPG"
+  },
+  {
+    "revision": "8bcea5dabda35901c0f870ce1446ac71",
+    "url": "/static/media/g6.8bcea5da.jpeg"
+  },
+  {
+    "revision": "3888bd5a40bad3cc484a0490c38d8a78",
+    "url": "/static/media/g7.3888bd5a.jpeg"
+  },
+  {
+    "revision": "190e2eaccfdf723517bfc5b7d6726fa8",
+    "url": "/static/media/g8.190e2eac.jpeg"
+  },
+  {
+    "revision": "e3fa278c35431e4164b709600a6a0cd3",
+    "url": "/static/media/g9.e3fa278c.jpeg"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "/static/media/getFetch.013f0b90.cjs"
+  },
+  {
+    "revision": "fd7326ef0e6cf7d955d460dbd5a4e7e2",
+    "url": "/static/media/himanshu.fd7326ef.jpg"
+  },
+  {
+    "revision": "a08f41d2fe59d7e81306d46cb517f74e",
+    "url": "/static/media/img3.a08f41d2.jpg"
+  },
+  {
+    "revision": "4fe29bbb0d21ebe4155351aa614e9085",
+    "url": "/static/media/jai balaji.4fe29bbb.png"
+  },
+  {
+    "revision": "ae5a6bc3de321502c0712b12339268ca",
+    "url": "/static/media/jindal.ae5a6bc3.png"
+  },
+  {
+    "revision": "28849a0e3c793053ab9cba1d99011caf",
+    "url": "/static/media/loading.28849a0e.svg"
+  },
+  {
+    "revision": "c1fd0695464e7061331f1d19bd8a5900",
+    "url": "/static/media/maa-mahamaya-industries-limited.c1fd0695.jpg"
+  },
+  {
+    "revision": "f525b3a06d2ddc1fccbad6ebee2c8be2",
+    "url": "/static/media/meltservice.f525b3a0.jpg"
+  },
+  {
+    "revision": "68edea7e84fde2243638d314ce80c6a3",
+    "url": "/static/media/metaflux.68edea7e.png"
+  },
+  {
+    "revision": "c70d848d8e6b4f8caacee0fc952bec8c",
+    "url": "/static/media/sanjay.c70d848d.JPG"
+  },
+  {
+    "revision": "a9adeba568053dc5153cf75747848303",
+    "url": "/static/media/sc.a9adeba5.jpeg"
+  },
+  {
+    "revision": "8496b3fe4ca28b19d26aa7df25aac556",
+    "url": "/static/media/scc.8496b3fe.jpeg"
+  },
+  {
+    "revision": "4cac7d0815bf5a2759e3f70edefee9ec",
+    "url": "/static/media/trith alloy.4cac7d08.png"
+  },
+  {
+    "revision": "2a36d8a622843c91348d6f304fb4a4bb",
+    "url": "/static/media/video.2a36d8a6.mp4"
+  }
+]);
